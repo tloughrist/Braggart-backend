b5ea21c7047eec67c40038ede637383609d4fc7f
timloughrist <tim.loughrist@gmail.com>
2022-10-29 18:13:44 -0500

    add seeds

--- db/migrate/20221029210808_create_matches.rb
+++ db/migrate/20221029210808_create_matches.rb
@@ -1,7 +1,7 @@
 class CreateMatches < ActiveRecord::Migration[6.1]
   def change
     create_table :matches do |t|
-      t.datetime :match_date
+      t.date :match_date
       t.integer :game_id
     end
   end
