90552f75beb64a0e633fca85c78ca910c52eb14a
timloughrist <tim.loughrist@gmail.com>
2022-10-30 11:21:07 -0500

    edit migrations

--- app/models/Game.rb
+++ app/models/Game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
     has_many :matches
-    has_many :player_matchse, through: :matches
+    has_many :player_matches, through: :matches
     has_many :players, through: :player_matches
 end
