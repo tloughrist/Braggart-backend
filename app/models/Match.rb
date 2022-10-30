886cf779ff8f7a170c95ff0cb588f941df4b4d1d
timloughrist <tim.loughrist@gmail.com>
2022-10-30 11:28:02 -0500

    migrate and seed

--- app/models/Match.rb
+++ app/models/Match.rb
@@ -1,5 +1,5 @@
 class Match < ActiveRecord::Base
     belongs_to :game
     has_many :player_matches
-    hase_many :players, through: :player_matches
+    has_many :players, through: :player_matches
 end
--- app/models/PlayerMatch.rb
+++ app/models/PlayerMatch.rb
@@ -1,5 +1,5 @@
 class PlayerMatch < ActiveRecord::Base
     belongs_to :match
     belongs_to :player
-    belongs_to :game, through: :match
+    #belongs_to :game, through: :match
 end
